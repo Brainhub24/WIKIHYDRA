89592c34313c9273338f8eaaab05ba3d71e0654e
Jan Gebser <600613.m6@gmail.com>
2024-03-19 23:37:33 +0100

    Update request_authors.py

--- request_authors.py
+++ request_authors.py
@@ -1,25 +1,41 @@
 import requests
 
-def get_authors(page_title, lang='en'):
+def get_all_authors(page_title, lang='en'):
     url = f'https://{lang}.wikipedia.org/w/api.php'
     params = {
         'action': 'query',
         'format': 'json',
         'prop': 'revisions',
         'titles': page_title,
-        'rvprop': 'user'
+        'rvprop': 'user|timestamp',
+        'rvlimit': 'max'  # Retrieve all revisions
     }
-    response = requests.get(url, params=params)
-    data = response.json()
-    pages = data['query']['pages']
-    page_id = list(pages.keys())[0]
-    revisions = pages[page_id]['revisions']
-    authors = [rev['user'] for rev in revisions]
+    authors = []
+    continue_flag = True
+
+    while continue_flag:
+        response = requests.get(url, params=params)
+        data = response.json()
+        pages = data['query']['pages']
+        page_id = list(pages.keys())[0]
+        revisions = pages[page_id]['revisions']
+        
+        for rev in revisions:
+            # Append author and timestamp as a tuple
+            authors.append((rev['user'], rev['timestamp']))
+
+        if 'continue' in data:
+            params['rvcontinue'] = data['continue']['rvcontinue']
+        else:
+            continue_flag = False
+
     return authors
 
 if __name__ == "__main__":
     page_title = 'Siegfried_Gebser'
-    authors = get_authors(page_title, lang='de')
+    # My beloved grandfather / my hero, I miss you...
+    # He was the living Wikipedia in his lifetime. 
+    authors = get_all_authors(page_title, lang='de')
     print("Authors of the Wikipedia page:")
-    for author in authors:
-        print(author)
+    for author, timestamp in authors:
+        print(f"{author} - {timestamp}")
