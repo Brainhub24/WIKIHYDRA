b0f91e0227ce2f8e2e402528d45819e9dde90924
Jan Gebser <600613.m6@gmail.com>
2024-03-20 00:03:05 +0100

    Update request_authors.py

--- request_authors.py
+++ request_authors.py
@@ -3,7 +3,7 @@
 #          Intro & Authors          #
 #####################################
 # Script: request_authors.py        #
-# Version: 1.3                      #
+# Version: 1.4                      #
 # Codename: WIKIHYDRA               #
 # Date: March 19, 2024              #
 #####################################
@@ -16,54 +16,59 @@ import requests
 from tabulate import tabulate
 import pyfiglet
 
-def get_all_authors(page_title, lang='en'):
-    url = f'https://{lang}.wikipedia.org/w/api.php'
-    params = {
-        'action': 'query',
-        'format': 'json',
-        'prop': 'revisions',
-        'titles': page_title,
-        'rvprop': 'user|timestamp',
-        'rvlimit': 'max'  # Retrieve all revisions
-    }
-    authors = []
-    continue_flag = True
+class WIKIHYDRA_Scanner:
+    def __init__(self, page_title, lang='en'):
+        self.page_title = page_title
+        self.lang = lang
 
-    while continue_flag:
+    def WIKIHYDRA_get_all_authors(self):
+        url = f'https://{self.lang}.wikipedia.org/w/api.php'
+        params = {
+            'action': 'query',
+            'format': 'json',
+            'prop': 'revisions',
+            'titles': self.page_title,
+            'rvprop': 'user|timestamp',
+            'rvlimit': 'max'  # Retrieve all revisions
+        }
+        authors = []
+        continue_flag = True
+
+        while continue_flag:
+            response = requests.get(url, params=params)
+            data = response.json()
+            pages = data['query']['pages']
+            page_id = list(pages.keys())[0]
+            revisions = pages[page_id]['revisions']
+            
+            for rev in revisions:
+                # Append author and timestamp as a tuple
+                authors.append((rev['user'], rev['timestamp']))
+
+            if 'continue' in data:
+                params['rvcontinue'] = data['continue']['rvcontinue']
+            else:
+                continue_flag = False
+
+        return authors
+
+    def WIKIHYDRA_get_article_intro(self):
+        url = f'https://{self.lang}.wikipedia.org/w/api.php'
+        params = {
+            'action': 'query',
+            'format': 'json',
+            'prop': 'extracts',
+            'titles': self.page_title,
+            'exintro': True,
+            'explaintext': True
+        }
+
         response = requests.get(url, params=params)
         data = response.json()
         pages = data['query']['pages']
         page_id = list(pages.keys())[0]
-        revisions = pages[page_id]['revisions']
-        
-        for rev in revisions:
-            # Append author and timestamp as a tuple
-            authors.append((rev['user'], rev['timestamp']))
-
-        if 'continue' in data:
-            params['rvcontinue'] = data['continue']['rvcontinue']
-        else:
-            continue_flag = False
-
-    return authors
-
-def get_article_intro(page_title, lang='en'):
-    url = f'https://{lang}.wikipedia.org/w/api.php'
-    params = {
-        'action': 'query',
-        'format': 'json',
-        'prop': 'extracts',
-        'titles': page_title,
-        'exintro': True,
-        'explaintext': True
-    }
-
-    response = requests.get(url, params=params)
-    data = response.json()
-    pages = data['query']['pages']
-    page_id = list(pages.keys())[0]
-    intro = pages[page_id]['extract']
-    return intro.strip()
+        intro = pages[page_id]['extract']
+        return intro.strip()
 
 if __name__ == "__main__":
     page_title = 'Siegfried_Gebser'
@@ -74,12 +79,15 @@ if __name__ == "__main__":
     ascii_title = pyfiglet.figlet_format(page_title)
     print(ascii_title)
 
+    # Create WIKIHYDRA scanner instance
+    scanner = WIKIHYDRA_Scanner(page_title, lang='de')
+
     # Get article introduction
-    intro = get_article_intro(page_title, lang='de')
+    intro = scanner.WIKIHYDRA_get_article_intro()
     print(intro)
 
     # Get authors
-    authors = get_all_authors(page_title, lang='de')
+    authors = scanner.WIKIHYDRA_get_all_authors()
 
     # Format output as a table
     headers = ["Author", "Timestamp"]
